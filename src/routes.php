eb5708e2bd50a3fe49fb223488645f496b6162d8
Francis Muturi <francmut@gmail.com>
2024-05-04 18:08:33 +0300

    Add google auth

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,8 @@
 
 use Fleetbase\FleetOps\Models\Contact;
 use Fleetbase\Storefront\Http\Resources\Customer;
+use Fleetbase\Storefront\Models\Network;
+use Fleetbase\Storefront\Models\Store;
 use Fleetbase\Storefront\Support\Storefront;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
@@ -11,7 +13,7 @@ Route::prefix(config('storefront.api.routing.prefix', 'storefront'))->group(
     function () {
 
         Route::prefix('v1')
-            ->middleware('storefront.api')
+            ->middleware('web')
             ->namespace('v1')
             ->group(function ($router) {
 
@@ -25,16 +27,11 @@ Route::prefix(config('storefront.api.routing.prefix', 'storefront'))->group(
                     return Response::json([ 'url' => $url ]);
                 });
 
-            });
-
-        Route::prefix('v1')
-            ->middleware('web')
-            ->namespace('v1')
-            ->group(function ($router) {
-
                 $router->get('/auth/facebook/callback', function() {
 
                     $facebookUser = Socialite::driver('facebook')->user();
+
+                    $key = config('services.fleetbase.storefront_key');
 
                     //
                     $user = Fleetbase\Models\User::firstOrCreate(
@@ -47,7 +44,11 @@ Route::prefix(config('storefront.api.routing.prefix', 'storefront'))->group(
                     );
 
                     // get the storefront or network login info
-                    $about = Storefront::about(['company_uuid']);
+                    if (\Str::startsWith($key, 'store')) {
+                        $about = $about = Store::select(['company_uuid'])->where('key', $key)->first();
+                    } else {
+                        $about = Network::select(['company_uuid'])->where('key', $key)->first();
+                    }
 
                     // get contact record
                     $contact = Contact::firstOrCreate(
@@ -75,15 +76,61 @@ Route::prefix(config('storefront.api.routing.prefix', 'storefront'))->group(
 
                     $contact->token = $token->plainTextToken;
 
-                    \Log::info('FacebookUser', [ $user->user['name'], $token ]);
-
                     return new Customer($contact);
 
                 });
 
                 $router->get('/auth/google/callback', function() {
-                    $user = Socialite::driver('google')->user();
-                    \Log::info('GoogleUser', [ $user ]);
+
+                    $googleUser = Socialite::driver('google')->user();
+
+                    $key = config('services.fleetbase.storefront_key');
+
+                    //
+                    $user = Fleetbase\Models\User::firstOrCreate(
+                        [
+                            'email' => $googleUser->user['email'],
+                        ],
+                        [
+                            'name' => $googleUser->user['name']
+                        ]
+                    );
+
+                    // get the storefront or network login info
+                    if (\Str::startsWith($key, 'store')) {
+                        $about = $about = Store::select(['company_uuid'])->where('key', $key)->first();
+                    } else {
+                        $about = Network::select(['company_uuid'])->where('key', $key)->first();
+                    }
+
+                    // get contact record
+                    $contact = Contact::firstOrCreate(
+                        [
+                            'user_uuid'    => $user->uuid,
+                            'company_uuid' => $about->company_uuid,
+                            'type'         => 'customer',
+                        ],
+                        [
+                            'user_uuid'    => $user->uuid,
+                            'company_uuid' => $about->company_uuid,
+                            'name'         => $googleUser->user['name'],
+                            'phone'        => null,
+                            'email'        => $googleUser->user['email'],
+                            'type'         => 'customer',
+                        ]
+                    );
+
+                    // get auth token
+                    try {
+                        $token = $user->createToken($contact->uuid);
+                    } catch (Exception $e) {
+                        return response()->errors($e->getMessage());
+                    }
+
+                    $contact->token = $token->plainTextToken;
+
+                    return new Customer($contact);
+
                 });
 
             });
